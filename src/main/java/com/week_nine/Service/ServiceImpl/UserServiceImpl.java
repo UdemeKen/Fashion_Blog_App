090a04801a56be5ced0f9fd7e702845642a54489
decagon <udemekendrick@gmail.com>
2022-12-23 07:42:11 +0100

    Create Admin Service Implementation Test Added.

--- src/main/java/com/week_nine/Service/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/week_nine/Service/ServiceImpl/UserServiceImpl.java
@@ -5,17 +5,13 @@ import com.week_nine.Exceptions.UserAlreadyExists;
 import com.week_nine.Model.User;
 import com.week_nine.Pojo.UserSignupDto;
 import com.week_nine.Repository.UserRepository;
-import com.week_nine.Service.UserServiceInterface;
-import lombok.Data;
+import com.week_nine.Service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpSession;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserServiceInterface {
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
--- src/main/java/com/week_nine/Service/UserService.java
+++ src/main/java/com/week_nine/Service/UserService.java
@@ -3,6 +3,6 @@ package com.week_nine.Service;
 import com.week_nine.Model.User;
 import com.week_nine.Pojo.UserSignupDto;
 
-public interface UserServiceInterface {
+public interface UserService {
     User createAdmin(UserSignupDto userSignupDto);
 }
